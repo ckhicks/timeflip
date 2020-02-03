ff000d88b843e87a7b5fc73b36498887a32574aa
ckhicks <contact@ckhicks.com>
2020-02-03 15:06:58 -0600

    asdf

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import noble from "noble";
+import noble from "@s524797336/noble-mac";
 import async from "async";
 import express from "express";
 import bodyParser from "body-parser";
@@ -6,8 +6,10 @@ import template from "./templates/main";
 
 const app = express();
 const TIMEFLIP = 'ffff544676332e3100';
-const FACET = 'f1196f5471a411e6bdf40800200c9a66';
-const BATTERY = '2a19';
+const FACETS = 'f1196f5071a411e6bdf40800200c9a66';
+const ACCELEROMETER = 'f1196f5171a411e6bdf40800200c9a66';
+const DEVICE = '180a';
+const BATTERY = '180f';
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -17,134 +19,31 @@ app.get('/', function (req, res) {
 });
 
 const connect = function (peripheral) {
-    peripheral.connect(function (error) {
-        peripheral.discoverServices([], function (error, services) {
-            var serviceIndex = 0;
-
-            async.whilst(
-                function () {
-                    return (serviceIndex < services.length);
-                },
-                function (callback) {
-                    var service = services[serviceIndex];
-                    var serviceInfo = service.uuid;
-
-                    if (service.name) {
-                        serviceInfo += ' (' + service.name + ')';
-                    }
-                    console.log(serviceInfo);
-
-                    service.discoverCharacteristics([], function (error, characteristics) {
-                        var characteristicIndex = 0;
-
-                        async.whilst(
-                            function () {
-                                return (characteristicIndex < characteristics.length);
-                            },
-                            function (callback) {
-                                var characteristic = characteristics[characteristicIndex];
-                                var characteristicInfo = '  ' + characteristic.uuid;
-                                if (characteristic.uuid == BATTERY) {
-                                    console.log('BATT');
-                                }
-                                if (characteristic.uuid == FACET) {
-                                    console.log('FACET');
-                                }
-                                if (characteristic.name) {
-                                    characteristicInfo += ' (' + characteristic.name + ')';
-                                }
-
-                                async.series([
-                                    function (callback) {
-                                        characteristic.discoverDescriptors(function (error, descriptors) {
-                                            async.detect(
-                                                descriptors,
-                                                function (descriptor, callback) {
-                                                    if (descriptor.uuid === '2901') {
-                                                        return callback(descriptor);
-                                                    } else {
-                                                        return callback();
-                                                    }
-                                                },
-                                                function (userDescriptionDescriptor) {
-                                                    if (userDescriptionDescriptor) {
-                                                        userDescriptionDescriptor.readValue(function (error, data) {
-                                                            if (data) {
-                                                                characteristicInfo += ' (' + data.toString() + ')';
-                                                            }
-                                                            callback();
-                                                        });
-                                                    } else {
-                                                        callback();
-                                                    }
-                                                }
-                                            );
-                                        });
-                                    },
-                                    function (callback) {
-                                        characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
-
-                                        if (characteristic.properties.indexOf('read') !== -1) {
-                                            characteristic.read(function (error, data) {
-                                                if (data) {
-                                                    var string = data.toString('ascii');
-
-                                                    characteristicInfo += '\n    value       ' + (characteristic.uuid == BATTERY ? parseInt(data.toString('hex'), 16) + '%' : data.toString('hex')) + ' | \'' + string + '\'';
-                                                }
-                                                callback();
-                                            });
-                                        } else {
-                                            callback();
-                                        }
-                                    },
-                                    function () {
-                                        console.log(characteristicInfo);
-                                        characteristicIndex++;
-                                        callback();
-                                    }
-                                ]);
-                            },
-                            function (error) {
-                                serviceIndex++;
-                                callback();
-                            }
-                        );
-                    });
-                },
-                function (err) {
-                    peripheral.disconnect();
-                }
-            );
+  peripheral.connect(function (error) {
+      peripheral.discoverServices([], function (error, services) {
+        services.forEach(service => {
+          if (service.uuid === FACETS) {
+            console.log(service._noble._characteristics[FACETS]);
+            // console.log(service._noble._events.stateChange);
+          }
         });
-    });
+      });
+  });
 };
 
 const discover = function (peripheral) {
     if (peripheral.advertisement.manufacturerData) {
         if (peripheral.advertisement.manufacturerData.toString('hex') == TIMEFLIP) {
-            console.log('\there is my manufacturer data:');
-            console.log('\t\t' + JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex')));
-            console.log('peripheral discovered (' + peripheral.id +
+            // console.log('\there is my manufacturer data:');
+            // console.log('\t\t' + JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex')));
+            console.log(peripheral.id +
                 ' with address <' + peripheral.address + ', ' + peripheral.addressType + '>,' +
                 ' connectable ' + peripheral.connectable + ',' +
                 ' RSSI ' + peripheral.rssi + ':');
-
-            console.log('\thello my local name is:');
-            console.log('\t\t' + peripheral.advertisement.localName);
-            console.log('\tcan I interest you in any of the following advertised services:');
-            console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
-            var serviceData = peripheral.advertisement.serviceData;
-            if (serviceData && serviceData.length) {
-                console.log('\there is my service data:');
-                for (var i in serviceData) {
-                    console.log('\t\t' + JSON.stringify(serviceData[i].uuid) + ': XXX ' + JSON.stringify(serviceData[i].data.toString('hex')));
-                }
-            }
-            if (peripheral.advertisement.txPowerLevel !== undefined) {
-                console.log('\tmy TX power level is:');
-                console.log('\t\t' + peripheral.advertisement.txPowerLevel);
-            }
-            console.log();
+            // if (peripheral.advertisement.txPowerLevel !== undefined) {
+            //     console.log('\tmy TX power level is:');
+            //     console.log('\t\t' + peripheral.advertisement.txPowerLevel);
+            // }
             connect(peripheral);
         }
     }
@@ -153,9 +52,10 @@ const discover = function (peripheral) {
 
 app.listen(3020, function () {
     console.log('Timeflip listening on port 3020');
-    noble.on('stateChange', function (state) {
+  noble.on('stateChange', function (state) {
+      console.log('------------------------ state change ------------------------');
         if (state === 'poweredOn') {
-            noble.startScanning();
+            noble.startScanning([], true);
         } else {
             noble.stopScanning();
         }
